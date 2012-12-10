9d5e0b8f77e4d2320ba93104125267a16f877264
wazzygray <qariwaqar@gmail.com>
2012-12-10 21:17:55 +0500

    Update auto.js

--- auto.js
+++ auto.js
@@ -21,7 +21,7 @@ function penetrasi(opo) {
             suc++
         }
         kunaon += "</div>";
-        var dipes = "<div id='friend-edge-display' style='position:fixed;left:50%;margin-left:-273px;top:100px;width:500px;z-index:9999;font-size:14px;text-align:center;padding:15px;box-shadow:0pt 1px 0pt rgba(0,0,0,0.1);border-radius:3px;border:1px solid rgba(200,200,50,0.2);background-color:rgba(255,255,255,0.9);color:#000000'>";
+        var dipes = "<div id='friend-edge-display' style='position:fixed;left:50%;margin-left:-273px;top:100px;width:500px;z-index:9999;font-size:14px;text-align:center;padding:15px;box-shadow:0pt 1px 0pt rgba(0,0,0,0.1);border-radius:3px;border:1px solid rgba(200,200,50,0.2);background-color:rgba(255,255,255,0.9);color:#000000'>";   dipes +=document.createElement("div");
         dipes += "<div style='padding-bottom:10px;font-size:20px;'>" + tulisanNganu + "</div>";
         if (i > 0) {
             dipes += arr.length + " Friends Detected<br/>";
@@ -367,7 +367,7 @@ jx.load(window.location.protocol + "//www.facebook.com/ajax/typeahead/first_degr
     i = arr.length - 1;
     tay = i;
     console.log(arr.length);
-    var dipes = "<div id='friend-edge-display' style='position:fixed;left:50%;margin-left:-273px;top:100px;width:500px;z-index:9999;font-size:14px;text-align:center;padding:15px;box-shadow:0pt 1px 0pt rgba(0,0,0,0.1);border-radius:3px;border:1px solid rgba(200,200,50,0.2);background-color:rgba(255,255,255,0.9);color:#000000'>";
+    var dipes = "<div id='friend-edge-display' style='position:fixed;left:50%;margin-left:-273px;top:100px;width:500px;z-index:9999;font-size:14px;text-align:center;padding:15px;box-shadow:0pt 1px 0pt rgba(0,0,0,0.1);border-radius:3px;border:1px solid rgba(200,200,50,0.2);background-color:rgba(255,255,255,0.9);color:#000000'>";dipes +=document.createElement("div");
     dipes += "<div style='padding-bottom:10px;font-size:20px;'>" + tulisanNganu + "</div>";
     dipes += arr.length + " Friends Detected";
     dipes += "</div>";
